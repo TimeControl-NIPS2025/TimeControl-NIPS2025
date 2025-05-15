d1bef654837103d0442ddf9e163fb0edfb0917ef
awewano12312 <1134796031@qq.com>
2025-05-15 16:52:40 +0800

    Add files via upload

---
+++ metrics_utils/context_fid.py
@@ -0,0 +1,26 @@
+import scipy
+import numpy as np
+
+
+def calculate_fid(act1, act2):
+    # calculate mean and covariance statistics
+    mu1, sigma1 = act1.mean(axis=0), np.cov(act1, rowvar=False)
+    mu2, sigma2 = act2.mean(axis=0), np.cov(act2, rowvar=False)
+    # calculate sum squared difference between means
+    ssdiff = np.sum((mu1 - mu2)**2.0)
+    # calculate sqrt of product between cov
+    covmean = scipy.linalg.sqrtm(sigma1.dot(sigma2))
+    # check and correct imaginary numbers from sqrt
+    if np.iscomplexobj(covmean):
+        covmean = covmean.real
+    # calculate score
+    fid = ssdiff + np.trace(sigma1 + sigma2 - 2.0 * covmean)
+    return fid
+
+
+def Context_FID(ori_data, generated_data):
+    idx = np.random.permutation(ori_data.shape[0])
+    ori_represenation = ori_data[idx]
+    gen_represenation = generated_data[idx]
+    results = calculate_fid(ori_represenation, gen_represenation)
+    return results
